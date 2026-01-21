b5e17e3516612b659b39996949bffbcf2caa421e
Juan Santos <juan.moreira62@outlook.com>
2026-01-21 18:09:10 -0300

    installing axios

--- FinTrack/src/pages/Home/Api.jsx
+++ FinTrack/src/pages/Home/Api.jsx
@@ -2,16 +2,28 @@
 // conectando back com front
 
 import { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const Api = () => {
     const [message, setMessage] = useState('carregando...');
+    
     useEffect(() => {
-        fetch('http://localhost:3000/').then((res) => res.json()).then((data) => {
-            setMessage(data.message);
-        }).catch(err => {
-            setMessage('Erro ao conectar com a API');
-            setMessage('Erro ao conectar com a API', err);
-        }, []);
+        // testando axios
+        axios.get('http://localhost:3000/')
+            .then((response) => {
+                setMessage(response.data.message)
+            })
+            .catch((error) => {
+                console.error("O erro foi:", error)
+                setMessage("faltou a API aqui veinho")
+            });
+
+        // fetch('http://localhost:3000/').then((res) => res.json()).then((data) => {
+        //     setMessage(data.message);
+        // }).catch(err => {
+        //     setMessage('Erro ao conectar com a API');
+        //     setMessage('Erro ao conectar com a API', err);
+        // }, []);
     }, []);
 
     return (
