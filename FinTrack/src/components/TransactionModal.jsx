2b5b0d107b244dcb1f51b19d78b08f2fdd6239e3
Juan Santos <juan.moreira62@outlook.com>
2026-02-13 19:34:39 -0300

    feat: add responsiveness to fintrack page and modal

--- FinTrack/src/components/TransactionModal.jsx
+++ FinTrack/src/components/TransactionModal.jsx
@@ -12,22 +12,22 @@ const TransactionModal = ({ isOpen, onClose, type, onSuccess }) => {
 
     try {
       const token = localStorage.getItem('token');
-      
-     
+
+
       await axios.post('http://localhost:3000/api/financeiro/add', {
         description,
-        value: Number(value), 
-        type 
+        value: Number(value),
+        type
       }, {
         headers: { Authorization: `Bearer ${token}` }
       });
 
-     
+
       setDescription('');
       setValue('');
-      
-      
-      onSuccess(); 
+
+
+      onSuccess();
       onClose(); // Fecha a janela
 
     } catch (error) {
@@ -36,24 +36,24 @@ const TransactionModal = ({ isOpen, onClose, type, onSuccess }) => {
   };
 
   return (
-    
+
     <div className="fixed inset-0 bg-black/80 flex justify-center items-center z-50 backdrop-blur-sm">
-      
+
       {/*  Modal */}
-      <div className={`bg-gray-800 p-8 rounded-2xl w-96 border-2 ${type === 'income' ? 'border-emerald-500' : 'border-red-500'} shadow-2xl`}>
-        
-        <h2 className="text-2xl font-bold text-white mb-6 uppercase tracking-wider text-center">
+      <div className={`bg-gray-800 md:p-8 p-4 rounded-2xl md:w-96 w-56 border-2 ${type === 'income' ? 'border-emerald-500' : 'border-red-500'} shadow-2xl`}>
+
+        <h2 className="md:text-2xl text-lg font-bold text-white mb-2 md:mb-6 uppercase tracking-wider text-center">
           Nova {type === 'income' ? <span className="text-emerald-400">Receita </span> : <span className="text-red-500">Despesa </span>}
         </h2>
 
         <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-          
+
           <div>
             <label className="text-gray-400 text-sm ml-1">Descrição</label>
-            <input 
-              type="text" 
-              placeholder="Ex: Salário, Pizza..." 
-              className="w-full bg-gray-900 text-white border border-gray-700 rounded-lg p-3 focus:outline-none focus:border-blue-500 transition-colors"
+            <input
+              type="text"
+              placeholder="Ex: Salário, Pizza..."
+              className="w-full bg-gray-900 text-white border border-gray-700 rounded-lg md:p-3 p-1 focus:outline-none focus:border-blue-500 transition-colors"
               value={description}
               onChange={(e) => setDescription(e.target.value)}
               required
@@ -62,28 +62,28 @@ const TransactionModal = ({ isOpen, onClose, type, onSuccess }) => {
 
           <div>
             <label className="text-gray-400 text-sm ml-1">Valor (R$)</label>
-            <input 
-              type="number" 
-              placeholder="0.00" 
-              className="w-full bg-gray-900 text-white border border-gray-700 rounded-lg p-3 focus:outline-none focus:border-blue-500 transition-colors"
+            <input
+              type="number"
+              placeholder="0.00"
+              className="w-full bg-gray-900 text-white border border-gray-700 rounded-lg md:p-3 p-1 focus:outline-none focus:border-blue-500 transition-colors"
               value={value}
               onChange={(e) => setValue(e.target.value)}
               required
-              step="0.01" 
+              step="0.01"
             />
           </div>
 
-          <div className="flex gap-3 mt-4">
-            <button 
-              type="button" 
+          <div className="flex gap-3 md:mt-4 mt-2">
+            <button
+              type="button"
               onClick={onClose}
-              className="flex-1 bg-gray-700 hover:bg-gray-600 text-white py-3 rounded-xl font-bold transition-all"
+              className="flex-1 md:text-base text-sm  bg-gray-700 hover:bg-gray-600 text-white py-1.5 md:py-3 rounded-xl font-bold transition-all"
             >
               Cancelar
             </button>
-            <button 
-              type="submit" 
-              className={`flex-1 py-3 rounded-xl font-bold text-white transition-all ${type === 'income' ? 'bg-emerald-600 hover:bg-emerald-700' : 'bg-red-600 hover:bg-red-700'}`}
+            <button
+              type="submit"
+              className={`flex-1 text-sm md:text-base md:py-3 py-1.5 rounded-xl font-bold text-white transition-all ${type === 'income' ? 'bg-emerald-600 hover:bg-emerald-700' : 'bg-red-600 hover:bg-red-700'}`}
             >
               Salvar
             </button>
--- FinTrack/src/pages/Fintrack/fintrack.jsx
+++ FinTrack/src/pages/Fintrack/fintrack.jsx
@@ -11,45 +11,45 @@ const Fintrack = () => {
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalType, setModalType] = useState('income');
-  // const [transactions, setTransactions] = useState([]);
+  const [transactions, setTransactions] = useState([]);
 
 
-  
-    const fetchFinanceData = async () => {
-      try {
-        const token = localStorage.getItem('token')
 
-        if (!token) {
-          console.log('Sem token painho! Loga ai pa nois');
-          return
-        }
+  const fetchFinanceData = async () => {
+    try {
+      const token = localStorage.getItem('token')
 
-        const response = await axios.get('http://localhost:3000/api/financeiro/get', {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        })
-
-        const data = response.data
-        // setTransactions(data)
-
-        const totalIncome = data
-          .filter(item => item.type === 'income')
-          .reduce((acc, curr) => acc + curr.value, 0)
-
-        const totalExpense = data
-          .filter(item => item.type === 'expense')
-          .reduce((acc, curr) => acc + curr.value, 0)
-
-        setIncome(totalIncome)
-        setExpense(totalExpense)
-        setBalance(totalIncome - totalExpense)
-      } catch (error) {
-        console.error('Erro ao buscar seus dados veinho', error);
+      if (!token) {
+        console.log('Sem token painho! Loga ai pa nois');
+        return
       }
 
+      const response = await axios.get('http://localhost:3000/api/financeiro/get', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      const data = response.data
+      setTransactions(data)
+
+      const totalIncome = data
+        .filter(item => item.type === 'income')
+        .reduce((acc, curr) => acc + curr.value, 0)
+
+      const totalExpense = data
+        .filter(item => item.type === 'expense')
+        .reduce((acc, curr) => acc + curr.value, 0)
+
+      setIncome(totalIncome)
+      setExpense(totalExpense)
+      setBalance(totalIncome - totalExpense)
+    } catch (error) {
+      console.error('Erro ao buscar seus dados veinho', error);
     }
-    useEffect(() => {
+
+  }
+  useEffect(() => {
     fetchFinanceData()
   }, [])
 
@@ -63,7 +63,7 @@ const Fintrack = () => {
 
   return (
 
-    <div className='bg-gray-900 min-h-screen flex flex-col items-center text-white p-10'>
+    <div className='bg-gray-900 md:h-screen h-[100dvh] flex flex-col items-center text-white md:p-10 p-4 overflow-hidden '>
 
 
       <TransactionModal
@@ -74,46 +74,113 @@ const Fintrack = () => {
 
       />
 
-      <h1 className="text-5xl font-bold mb-6 text-gray-200 ">WELCOME TO YOUR
+      <h1 className="md:text-5xl text-lg text-center font-bold mb-6 text-gray-200 ">WELCOME TO YOUR
         <span className="text-blue-500"> FINTRACK </span>
         DASHBOARD</h1>
 
-      <div className='w-full max-w-4xl flex gap-6 flex-wrap md:flex-nowrap'>
+      <div className='w-full max-w-4xl flex gap-3 md:gap-6 flex-row shrink-0'>
         {/* Card de Ganhos */}
-        <div className='flex-1 bg-gray-800 border-t-4 border-emerald-500 h-48 rounded-xl flex flex-col justify-center items-center shadow-2xl'>
-          <h2 className='text-emerald-400 text-2xl font-bold uppercase tracking-[0.2em] mb-4'>Income</h2>
-          <div className='text-3xl font-bold text-white' id='income'>
+        <div className='flex-1 bg-gray-800 border-t-2 border-emerald-500 md:h-48 h-28 p-2 md:p-4 rounded-xl flex flex-col justify-center items-center shadow-2xl'>
+          <h2 className='text-emerald-400 text-sm md:text-2xl font-bold uppercase tracking-[0.15em] mb-1 md:mb-4'>Income</h2>
+          <div className='md:text-3xl text-lg font-bold text-white' id='income'>
             R$ {income.toFixed(2)}
           </div>
           <button
             onClick={() => handleOpenModal('income')}
-            className='mt-4 font-bold border-2 border-emerald-500 rounded-xl p-1.5 hover:bg-emerald-600 transition-all duration-300 cursor-pointer text-gray-200 '>ADD INCOME</button>
+            className='md:mt-4 mt-2 font-bold text-sm md:text-base border-2 border-emerald-500 rounded-xl p-1 md:p-1.5 hover:bg-emerald-600 transition-all duration-300 cursor-pointer text-gray-200 w-full max-w-[120px]'> INCOME</button>
         </div>
 
         {/* Card de Despesas */}
-        <div className='flex-1 bg-gray-800 border-t-4 border-red-500 h-48 rounded-xl flex flex-col justify-center items-center shadow-2xl'>
-          <h2 className='text-red-500 text-2xl font-bold uppercase tracking-[0.2em] mb-4'>Expenses</h2>
-          <div className='text-3xl font-black text-white' id='expenses'>
+        <div className='flex-1 p-2 bg-gray-800 border-t-2 border-red-500 md:h-48 h-28 md:p-4 rounded-xl flex flex-col justify-center items-center shadow-2xl'>
+          <h2 className='text-red-500 text-sm md:text-2xl font-bold uppercase tracking-[0.15em] mb-1 md:mb-4'>Expenses</h2>
+          <div className='md:text-3xl text-lg font-bold text-white' id='expenses'>
             R$ {expense.toFixed(2)}
           </div>
-          <button className='mt-4 font-bold border-2 border-red-500 hover:bg-red-700 transition-all duration-300 cursor-pointer rounded-xl p-1.5 text-gray-200 '
+          <button className='md:mt-4 mt-2 font-bold text-sm md:text-base border-2 border-red-500 hover:bg-red-700 transition-all duration-300 cursor-pointer rounded-xl p-1 md:p-1.5 text-gray-200 w-full max-w-[120px]'
             onClick={() => handleOpenModal('expense')}
-          >ADD EXPENSES</button>
+          >EXPENSES</button>
 
         </div>
       </div>
 
-      <div className='flex-1 flex items-center justify-center w-full'>
+      <div className='md:mt-10 mt-6 flex items-center justify-center w-full'>
         {/* Card de Balanço  */}
-        <div className='w-[40%] bg-gray-800 border border-blue-500/50 h-56 rounded-2xl flex flex-col justify-center items-center '>
-          <h2 className='text-blue-400 text-3xl font-bold uppercase tracking-[0.3em] mb-6'>Balance</h2>
-          <div className={`text-4xl font-black ${balance >= 0 ? 'text-blue-50' : 'text-red-500'}`} id='balance'>
+        <div className='md:w-[40%] w-full bg-gray-800 border border-blue-500/50 md:h-56 h-28 rounded-2xl flex flex-col justify-center items-center shrink-0 '>
+          <h2 className='text-blue-400 md:text-3xl text-lg font-bold uppercase tracking-[0.1em] mb-1 md:mb-6'>Balance</h2>
+          <div className={`md:text-4xl text-2xl font-bold ${balance >= 0 ? 'text-blue-50' : 'text-red-500'}`} id='balance'>
             R$ {balance.toFixed(2)}
           </div>
         </div>
       </div>
 
       {/* --- LISTA DE HISTÓRICO --- */}
+
+      <div className="w-full max-w-4xl mt-6 md:mt-12 flex-1 min-h-0 flex flex-col px-2 md:px-0">
+
+        <div className="flex justify-between items-center mb-4">
+          <h3 className="text-xl md:text-2xl font-bold text-gray-300">Histórico</h3>
+          <span className="text-gray-500 text-sm">Recentes</span>
+        </div>
+        <div className="bg-gray-800 rounded-xl shadow-xl border border-gray-700 flex flex-col flex-1 overflow-hidden">
+          <div className="overflow-x-auto overflow-y-auto flex-1
+    [&::-webkit-scrollbar-track]:bg-gray-800 
+    [&::-webkit-scrollbar-thumb]:bg-gray-600 
+    [&::-webkit-scrollbar-thumb]:rounded-full 
+    [scrollbar-width:thin] 
+    [scrollbar-color:#4B5563_transparent]
+    ">
+            <table className="w-full text-left border-collapse min-w-[500px]">
+
+              <thead className="sticky top-0 z-10 bg-gray-900 border-b border-gray-700">
+                <tr className="text-gray-400 text-[10px] md:text-xs uppercase md:tracking-wider">
+                  <th className="p-3 md:p-4 font-semibold whitespace-nowrap">Data</th>
+                  <th className="p-3 md:p-4 font-semibold">Descrição</th>
+                  <th className="p-3 md:p-4 font-semibold text-center">Tipo</th>
+                  <th className="p-3 md:p-4 font-semibold text-right">Valor</th>
+                </tr>
+              </thead>
+
+              <tbody className="divide-y divide-gray-700 text-xs md:text-sm">
+                {transactions.length === 0 ? (
+                  <tr>
+                    <td colSpan="4" className="p-8 text-center text-gray-500 text-sm md:text-base">
+                      Sem dados ainda, Painho...
+                    </td>
+                  </tr>
+                ) : (
+                  transactions.map((item) => (
+                    <tr key={item._id} className="hover:bg-gray-700/50 transition-colors">
+
+                      <td className="p-3 md:p-4 text-gray-400 whitespace-nowrap">
+                        {new Date(item.date || item.createdAt).toLocaleDateString('pt-BR')}
+                      </td>
+
+                      <td className="p-3 md:p-4 font-medium text-white">
+                        {item.description}
+                      </td>
+
+                      <td className="p-3 md:p-4 text-center">
+                        <span className={`px-2 py-1 rounded text-[9px] md:text-[10px] font-bold uppercase tracking-wide border whitespace-nowrap ${item.type === 'income'
+                          ? 'bg-emerald-500/10 text-emerald-400 border-emerald-500/20'
+                          : 'bg-red-500/10 text-red-400 border-red-500/20'
+                          }`}>
+                          {item.type === 'income' ? 'Income' : 'Expense'}
+                        </span>
+                      </td>
+
+                      <td className={`p-3 md:p-4 text-right font-bold text-sm md:text-base whitespace-nowrap ${item.type === 'income' ? 'text-emerald-400' : 'text-red-400'
+                        }`}>
+                        {item.type === 'income' ? '+' : '-'} R$ {item.value.toFixed(2)}
+                      </td>
+
+                    </tr>
+                  ))
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
 
 
     </div >
